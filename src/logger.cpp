520a487a71e66990fa3c029dd04a33b6ebdfab73
justin <justin1726hernandez2@gmail.com>
2026-03-22 23:53:36 -0400

    csv creation, logging, and  closing implementation

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,1 +1,27 @@
 #include "logger.hpp"
+#include <fstream>
+#include <iostream>
+#include <ostream>
+
+Logger::Logger() {
+    save_file.open("data.csv", std::ios::app);
+}
+
+Logger::~Logger() {
+    if (save_file.is_open()) {
+        save_file.close();
+    }
+
+}
+
+void Logger::logCSV(boost::asio::ip::address ip_address, std::string service, std::string time) {
+
+    if (!save_file.is_open()) {
+        std::cerr <<  "ERROR: couldn't open storage file";
+        return;
+    }
+
+    save_file << ip_address << "," << service << "," << time << "\n";
+
+
+}
--- src/logger.hpp
+++ src/logger.hpp
@@ -1,7 +1,18 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
+#include <boost/asio/ip/address.hpp>
+#include <boost/asio/ip/address.hpp>
+#include <string>
+#include <fstream>
+
 class Logger {
+    std::ofstream save_file;
+    
+public:
+    Logger();
+    ~Logger();
+    void logCSV(boost::asio::ip::address arg_ip_address, std::string arg_service, std::string arg_time);
 
 };
 
